58d7e94324d74b9ce7ea9db89a0e3e619dc727a3
KatieG <katieshake27@gmail.com>
2021-12-08 18:16:50 -0500

    working html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+
+const path = require('path')
+const router = require('express').Router();
+//localhost:3001/notes
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+//localhost:3001/anything
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
-
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
+
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
 
-const PORT = process.env.PORT || 3001;
+
 
 
 app.listen(PORT, () => {
