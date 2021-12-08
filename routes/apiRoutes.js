5ea40b4f4e282bf25ea14c521a7f50cdd8a4eba9
KatieG <katieshake27@gmail.com>
2021-12-08 18:21:39 -0500

    added git ignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
-module.exports = router
+
+//get route to get all the notes
+//POST route to post a new note
+//delete route to delete a note
+
+
+
+
+module.exports = router;
